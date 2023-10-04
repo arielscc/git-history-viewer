6f5a3b02cf69ecabeb66f7aa3a8fcb7ad5a45f8c
Ariel Chura <ariel.chura.c@gmail.com>
2023-10-03 20:39:44 -0400

    feat(app-module): add imports, controller and providers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CommitsController } from './commits/commits.controller';
+import { AppConfig } from './config/app.config';
+import { AuthService } from './github/auth.service';
+import { GithubService } from './github/github.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
+  controllers: [CommitsController],
+  providers: [GithubService, AuthService, AppConfig],
 })
 export class AppModule {}
