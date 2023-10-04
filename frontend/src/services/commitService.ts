b3fe5c154cd1b13349602e6fe366a908a5cbc37d
Ariel Chura <ariel.chura.c@gmail.com>
2023-10-04 18:51:32 -0400

    feat(commit-service): implementt commit service to get the commits

---
+++ frontend/src/services/commitService.ts
@@ -0,0 +1,42 @@
+import { AxiosError } from 'axios';
+import { GithubCommit } from 'interfaces/services/commit.interface';
+import { commitInstance } from './axiosService';
+
+export const fetchCommits = async (
+  url: string,
+  page: number
+): Promise<GithubCommit[] | string> => {
+  try {
+    const regex = /https:\/\/github\.com\/([^/]+)\/([^/]+)\/?$/;
+    const testUrl = regex.test(url);
+
+    if (!testUrl) {
+      throw new Error('Invalid URL');
+    }
+
+    const match = url.match(regex);
+    const user = match?.[1];
+    const repo = match?.[2];
+
+    const response = await commitInstance.get(`/commits/${user}/${repo}`, {
+      params: {
+        page: page,
+        perPage: 5
+      }
+    });
+
+    if (response.status !== 200) {
+      throw new Error('Error fetching commits');
+    }
+
+    return response.data;
+  } catch (error: unknown) {
+    if (error instanceof AxiosError) {
+      return error.response?.data.message;
+    }
+    if (error instanceof Error) {
+      return error.message;
+    }
+    return 'Unknown error, please try again';
+  }
+};
