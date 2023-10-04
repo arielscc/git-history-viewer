b0b3fe2a0c3ec2f712014fedf2bbb5576723a64b
Ariel Chura <ariel.chura.c@gmail.com>
2023-10-04 19:36:55 -0400

    feat(components): add header & footer

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+  return (
+    <div className="flex items-center justify-center mt-12 text-gray-300">
+      developed by
+      <a
+        href="https://arielchura.com"
+        target="_blank"
+        rel="noreferrer"
+        className="text-blue-500 ml-1"
+      >
+        @arielchura
+      </a>
+    </div>
+  );
+};
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl pb-12">
+      Git History Viewer
+    </h1>
+  );
+};
+
+export default Header;
