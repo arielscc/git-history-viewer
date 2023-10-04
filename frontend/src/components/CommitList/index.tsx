285d2f4e4bcb9f15c6bd3985a472104b464b44eb
Ariel Chura <ariel.chura.c@gmail.com>
2023-10-04 19:39:18 -0400

    feat(commit): add commit list component

---
+++ frontend/src/components/CommitList/index.tsx
@@ -0,0 +1,26 @@
+import { Commit } from 'components/Commit';
+import { GithubCommit } from 'interfaces/services/commit.interface';
+import { FC } from 'react';
+
+type CommitProps = {
+  commits: GithubCommit[];
+  lastItemRef: (node: HTMLDivElement) => void;
+};
+
+export const CommitList: FC<CommitProps> = ({ commits, lastItemRef }) => {
+  return (
+    <div className="relative border-l border-gray-200 dark:border-gray-700">
+      {commits.map((commit, index) => (
+        <div key={commit.sha}>
+          {commits.length === index + 1 ? (
+            <div ref={lastItemRef}>
+              <Commit data={commit} />
+            </div>
+          ) : (
+            <Commit data={commit} />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
